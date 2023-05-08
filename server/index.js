65cae9b47fa76bf518765a2a115dabc20ed9550e
najeebmosab <73877639+najeebmosab@users.noreply.github.com>
2023-05-08 13:09:31 +0300

    fix method put get post delete

--- server/index.js
+++ server/index.js
@@ -2,17 +2,16 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import http from "http";
-
+import waterRoute from "./Routes/Water.Routes.js";
 const app = express();
 const server = http.createServer(app);
-
 import path from "path";
 import { fileURLToPath } from "url";
-
+// app.use(express.bodyParser());
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 app.use(cors());
-app.use(express.json({ limit: "10mb" }));
+app.use(express.json());
 app.use(
   express.urlencoded({
     limit: "10mb",
@@ -20,6 +19,7 @@ app.use(
     parameterLimit: 50000000,
   })
 );
+app.use("/WaterData", waterRoute);
 
 const CONNECTION_URL = `mongodb+srv://waterproject:t2k4VkJn963j7guP@water.wn7b7lm.mongodb.net/`;
 const PORT = process.env.PORT || 5000;
