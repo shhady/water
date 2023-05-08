825e4d14f3edad33a20037c1fb3673848166e87a
SuperSpawn <thearktitan@gmail.com>
2023-05-08 19:01:48 +0300

    Redone the main problem schema

--- server/Model/MainWaterProblem.Model.js
+++ server/Model/MainWaterProblem.Model.js
@@ -1,17 +1,19 @@
 import mongoose from "mongoose";
 
 const mainWaterProblemSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    // Reference to sub-problems schema
-    subProblems: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'SubProblem'
-    }]
+  name: {
+    type: String,
+    required: true,
+  },
+  // Reference to sub-problems schema
+  subProblems: {
+    type: [String],
+  },
 });
 
-const MainWaterProblem = mongoose.model("mainWaterProblem", mainWaterProblemSchema);
+const MainWaterProblem = mongoose.model(
+  "mainWaterProblem",
+  mainWaterProblemSchema
+);
 
 export default MainWaterProblem;
--- server/Routes/MainWaterProblem.Route.js
+++ server/Routes/MainWaterProblem.Route.js
@@ -1,13 +1,25 @@
 import express from "express";
 const router = express.Router();
-import { createMainWaterProblem, getAllMainWaterProblems, updateMainWaterProblem, deleteMainWaterProblem } from "../Controller/MainWaterProblem.Controller.js";
-
+import {
+  createMainWaterProblem,
+  getAllMainWaterProblems,
+  updateMainWaterProblem,
+  deleteMainWaterProblem,
+  addSubProblem,
+  removeSubProblem,
+} from "../Controller/MainWaterProblem.Controller.js";
 
 // Create a new main water problem with sub-problems
 router.post("/", createMainWaterProblem);
 
 // Get all main water problems
 router.get("/", getAllMainWaterProblems);
+
+// Add a new sub-problem
+router.put("/add-sub-problem/:id", addSubProblem);
+
+// Add a new sub-problem
+router.put("/remove-sub-problem/:id", removeSubProblem);
 
 // Update a main water problem
 router.put("/:id", updateMainWaterProblem);
